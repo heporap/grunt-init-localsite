45c3a2645633ae7ecfdf687c776556b557100604
heporap <dab@wi-wi.jp>
2014-02-04 13:20:10 +0900

    first commit

---
+++ root/Gruntfile.js
@@ -0,0 +1,98 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    clean: {
+      build: ["dest/<%= pkg.name %>/"],
+      public_html: ['<%= pkg.public_html %>'],
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js', 'src/*.js'],
+        dest: 'dest/<%= pkg.name %>.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dest/<%= pkg.name %>.min.js'
+      },
+    },
+    copy: {
+      public_html: {
+        files: [
+          {expand: true, cwd: 'public_html/', src: ['**'], dest: '<%= pkg.public_html %>'},
+          {expand: true, cwd: 'dest/', src: ['**'], dest: '<%= pkg.public_html %>/dest'}
+        ]
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        options: {
+          jshintrc: 'lib/.jshintrc'
+        },
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'clean:build', 'concat', 'uglify']);
+  
+  // Custom tasks.
+  grunt.registerTask('public_html', ['clean:public_html', 'copy:public_html']);
+  grunt.registerTask('public_build', ['default', 'public_html']);
+
+};
